8b2815687beb4f8829e7f770bc85a90fed6b5b41
GrayFrazier <grayson@frazierbunch.com>
2018-10-05 15:47:59 -0400

    Adding organization and refining square borders

--- VoxelBorders.py
+++ VoxelBorders.py
@@ -9,10 +9,23 @@ def order_square_data:
     This is already assumed in the files, but just to be safe :)
     Assumes all z are the same
     '''
-    snp = sorted(snp , key=lambda k: [k[1], k[0], k[3]]) #also sorts by triangle orientation (row, column, then orientation)
+    snp = sorted(snp , key=lambda k: [k[1], k[0], k[3]]) #also sorts by triangle orientation (column, ro, then orientation)
     return snp
 
-def square_borders(SquareMic, SquareMicData):
+class border():
+    '''
+    simple class just to make things prettier
+    in the form
+    [[x1,y1], [x2,y2], left/up, right/down]
+    '''
+    def __init__(self, point1, point2, luvoxel = None, rdvoxel=None):
+        self.point1 = point1
+        self.point2 = point2
+        self.luvoxel = luvoxel
+        self.rdvoxel = rdvoxel
+
+
+def make_square_borders(SquareMic, SquareMicData):
     '''
     Returns a collection of borders
     Includes the two points, and the left/up voxel and the right/down voxel
@@ -25,5 +38,18 @@ def square_borders(SquareMic, SquareMicData):
         9: additional information
     In the form [(x,y), (x,y), left/up, right/down]
     '''
+    #Data organized by column, row, then orientation
 
-    
+    def make_left_side(SquareMic, smd0):
+        '''
+        Make left wall
+        requires left smd
+        '''
+        left_borders = np.array()
+        for i in smd0:
+            point1 = [smd[i][0], smd[i][1], smd[i][2]]
+            point2 = [smd[i][0], smd[i][1], smd[i][2]]
+            np.append(left_borders, point1, point2, None, smd[i])
+        return left_borders
+
+    def make_right_side(SquareMic, smdf)
